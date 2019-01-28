27e7ab9f8ccaff218fb8d62dfe99e5a78636b006
Christopher Harty <ascii1011@gmail.com>
2019-01-28 00:54:16 -0500

    added first base docker file.

---
+++ build/docker/images/base_image/download_files.sh
@@ -0,0 +1,6 @@
+
+# downloads epel-release, which is an update package that centos7 needs
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+# downloads anaconda version3, which will included and default to python 3.7.1
+wget https://repo.continuum.io/archive/Anaconda3-2018.12-Linux-x86_64.sh
---
+++ build/docker/images/base_image/rebuild_image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+IMAGE_NAME="cent76_conda3"
+CONTAINER_NAME="airflow1101"
+
+# remove previous containers first
+echo "docker ps -a | grep $CONTAINER_NAME"
+docker ps -a | grep $CONTAINER_NAME
+AF_CONT=$(docker ps -a | grep $CONTAINER_NAME)
+echo "AF_CONT: $AF_CONT"
+if [ -z AF_CONT ]
+then
+    ARR=($AF_CONT)
+    echo "ARR: $ARR"
+    CONTAINER_ID=${ARR[0]}
+    echo "CONT ID: $CONTAINER_ID"
+    if [ -z CONTAINER_ID ]
+    then
+	echo "attempting to stop and rum container"
+	docker stop $CONTAINER_ID
+	docker rm $CONTAINER_ID
+    fi
+fi
+
+
+docker build --tag $IMAGE_NAME .
+
