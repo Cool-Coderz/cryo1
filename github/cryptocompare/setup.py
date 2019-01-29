a13aa16dc46f4337574e25e4e3d54998c8a735af
Paul <pauldmendes5@gmail.com>
2019-01-29 14:27:19 -0500

    Add files via upload

---
+++ github/cryptocompare/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.rst', encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+        name='cryptocompare',
+        version='0.6.1',
+        description='Wrapper for CryptoCompare.com',
+        long_description=readme,
+        url='https://github.com/lagerfeuer/cryptocompare',
+        author='lagerfeuer',
+        author_email='lukas.deutz@mailfence.com',
+        keywords='crypto cryptocurrency wrapper cryptocompare',
+        license='MIT',
+        python_requires='>=3',
+        packages=['cryptocompare'],
+        classifiers=['Programming Language :: Python :: 3'],
+        install_requires=['requests']
+        )
